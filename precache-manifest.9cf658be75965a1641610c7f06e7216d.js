5a539b08936d59186ada8d91c75a541dde08f99a
Alexander Barouch <ajbarouch@gmail.com>
2020-07-15 16:56:28 -0700

    Updates

---
+++ precache-manifest.9cf658be75965a1641610c7f06e7216d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e976ea23f2254a08e69aed05511cb7a",
+    "url": "/webnarok-react/index.html"
+  },
+  {
+    "revision": "01bed341334b4dc60ff2",
+    "url": "/webnarok-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "68847467decef9849af3",
+    "url": "/webnarok-react/static/js/2.0da72a9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webnarok-react/static/js/2.0da72a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01bed341334b4dc60ff2",
+    "url": "/webnarok-react/static/js/main.f0b1ab1d.chunk.js"
+  },
+  {
+    "revision": "3121e3e2c41404c185d8",
+    "url": "/webnarok-react/static/js/runtime-main.ce1c35db.js"
+  }
+]);
