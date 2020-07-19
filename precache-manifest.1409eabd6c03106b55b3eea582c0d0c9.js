678aec5e220ee993da7f2a7df769c6918386a519
Alexander Barouch <ajbarouch@gmail.com>
2020-07-19 09:17:10 -0700

    Updates

---
+++ precache-manifest.1409eabd6c03106b55b3eea582c0d0c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d75c8dfb4c95408f3f60ebaf38b5d7",
+    "url": "/webnarok-react/index.html"
+  },
+  {
+    "revision": "681697ef8815434eb243",
+    "url": "/webnarok-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "68847467decef9849af3",
+    "url": "/webnarok-react/static/js/2.0da72a9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webnarok-react/static/js/2.0da72a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681697ef8815434eb243",
+    "url": "/webnarok-react/static/js/main.1225a3d8.chunk.js"
+  },
+  {
+    "revision": "3121e3e2c41404c185d8",
+    "url": "/webnarok-react/static/js/runtime-main.ce1c35db.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f6895b7aef9aa8cc947dc7f05ff2fa3.js"
+  "/webnarok-react/precache-manifest.1409eabd6c03106b55b3eea582c0d0c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webnarok-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
