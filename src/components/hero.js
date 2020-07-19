a1ab120a072468d3f739a609e0aaf2cf5bcf8a15
Alexander Barouch <ajbarouch@gmail.com>
2020-07-19 08:00:42 -0700

    deploy 1

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,8 +6,8 @@ export default class Hero extends Component {
       return (
         <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
         <div className="container text-center text-md-left" data-aos="fade-up">
-        <h1>Welcome to Maxim</h1>
-        <h2>We are team of talanted designers making websites with Bootstrap</h2>
+        <h1>Welcome to Webnarok</h1>
+        <h2>Talented designers making websites with Bootstrap.</h2>
         <a href="#about" className="btn-get-started scrollto">Get Started</a>
         </div>
         </section>
